253e7708ae785864fda2bca055c32c0c3a32582e
luozhouyang <stupidme.me.lzy@gmail.com>
2018-06-22 14:44:11 +0800

    Add setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pystrsim",
+    version="0.0.1",
+    description="A library implementing different string similarity and distance measures",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/luozhouyang/python-string-similarity",
+    author="ZhouYang Luo",
+    author_email="stupidme.me.lzy@gmail.com",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=["numpy>=1.14.3"],
+    license="MIT License",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    )
+)
---
+++ similarity/__init__.py
@@ -0,0 +1,1 @@
+__name__ = 'pystrsim'
