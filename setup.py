cbcba025f609f21f8e4d23a2057f0a338deb4524
luozhouyang <zhouyang.luo@gmail.com>
2019-10-05 17:48:52 +0800

    Upload v0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="strsimpy",
-    version="0.1.1",
+    version="0.1.2",
     description="A library implementing different string similarity and distance measures",
     long_description=long_description,
     long_description_content_type="text/markdown",
