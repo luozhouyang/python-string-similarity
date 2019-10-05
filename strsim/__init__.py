f04838c5fc05ebd7af06dcf69fef5281c853abaf
luozhouyang <zhouyang.luo@gmail.com>
2019-10-05 16:51:46 +0800

    Add imports

--- strsim/__init__.py
+++ strsim/__init__.py
@@ -18,4 +18,21 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+
+from .cosine import Cosine
+from .damerau import Damerau
+from .jaccard import Jaccard
+from .levenshtein import Levenshtein
+from .longest_common_subsequence import LongestCommonSubsequence
+from .metric_lcs import MetricLCS
+from .ngram import NGram
+from .normalized_levenshtein import NormalizedLevenshtein
+from .optimal_string_alignment import OptimalStringAlignment
+from .qgram import QGram
+from .shingle_based import ShingleBased
+from .sorensen_dice import SorensenDice
+from .string_distance import StringDistance
+from .string_similarity import StringSimilarity
+from .weighted_levenshtein import WeightedLevenshtein
+
 __name__ = 'strsim'
