d2fe06ae42ed44d4a9ed364cbee57594a31e7499
luozhouyang <zhouyang.luo@gmail.com>
2020-06-11 16:15:45 +0800

    Add python versions

--- setup.py
+++ setup.py
@@ -18,6 +18,10 @@ setuptools.setup(
     license="MIT License",
     classifiers=(
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     )
