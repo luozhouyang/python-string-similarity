396347c7c9ca803f53045cec7bfdad1b4bc8fd1b
luozhouyang <zhouyang.luo@gmail.com>
2020-07-03 10:45:17 +0800

    Bump version to 0.1.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="strsimpy",
-    version="0.1.6",
+    version="0.1.7",
     description="A library implementing different string similarity and distance measures",
     long_description=long_description,
     long_description_content_type="text/markdown",
